d7458bb14aab7d3d2f8937d4f28d03d2f64410a4
Mgc77 <57840741+Mgc77@users.noreply.github.com>
2022-12-20 23:00:03 +0000

    Add files via upload

---
+++ myapplicationforuni/CalenderViewHolder.java
@@ -0,0 +1,27 @@
+package com.example.myapplicationforuni;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.view.menu.MenuView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CalenderViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+    public final TextView dayOfMonth;
+    private final CalenderAdapter.OnItemListener onItemListener;
+    public CalenderViewHolder(@NonNull View itemView, CalenderAdapter.OnItemListener onItemListener) {
+        super(itemView);
+        dayOfMonth = itemView.findViewById((R.id.cellDayText));
+        this.onItemListener = onItemListener;
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        onItemListener.onItemClick(getAdapterPosition(),(String) dayOfMonth.getText());
+
+    }
+}
